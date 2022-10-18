62d551d31b2916e3c7eecb4c824c37cb2958aa15
calmisential <xinyu_std@163.com>
2022-10-18 16:06:14 +0800

    训练过程中计算准确率

--- train.py
+++ train.py
@@ -21,23 +21,30 @@ def train_loop(cfg, model, dataloader, device):
     # 损失函数
     loss_fn = cross_entropy_loss()
     loss_mean = MeanMetric()
+    correct_mean = MeanMetric()    # 一个epoch的平均正确率
 
     for epoch in range(epochs):
         with tqdm(dataloader, desc="Epoch-{}/{}".format(epoch, epochs)) as pbar:
             for i, (images, targets) in enumerate(pbar):
+                batch_size = images.size(0)
                 images = images.to(device)
                 targets = targets.to(device)
 
                 optimizer.zero_grad()
                 preds = model(images)
+
                 loss = loss_fn(preds, targets)
                 loss_mean.update(loss.item())
+
+                correct_mean.update((preds.argmax(1) == targets).type(torch.float).sum().item() / batch_size)
 
                 loss.backward()
                 optimizer.step()
 
-                pbar.set_postfix({"loss": "{}".format(loss_mean.result())})
+                pbar.set_postfix({"loss": "{}".format(loss_mean.result()),
+                                  "accuracy": "{:.4f}%".format(100 * correct_mean.result())})
         loss_mean.reset()
+        correct_mean.reset()
 
         if epoch % save_frequency == 0:
             torch.save(model.state_dict(), Path(save_path).joinpath("{}_epoch-{}.pth".format(model.model_name, epoch)))
